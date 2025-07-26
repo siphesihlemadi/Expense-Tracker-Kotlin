9eb612f447e4c1d62b7e44c87156ee55c73bf794
Siphesihle Madi <132097129+quadrills@users.noreply.github.com>
2025-07-26 18:54:56 +0200

    replace map with data class to store expense data

--- expenseTrackerKotlin/src/Main.kt
+++ expenseTrackerKotlin/src/Main.kt
@@ -1,7 +1,8 @@
 import java.time.LocalDate
 import java.time.format.DateTimeParseException
 
-var expenses = mutableListOf<MutableMap<String, String>>()
+data class Expense(val description: String, val amount: Double, val category: String, val date: LocalDate)
+var expenses = mutableListOf<Expense>()
 
 fun main() {
     var userChoice = 0
@@ -55,37 +56,29 @@ fun addExpense() {
     val date = readln().takeIf { it.isNotBlank() } ?: throw NullPointerException("Date cannot be empty.")
     val parsedDate = LocalDate.parse(date)
 
-    val expense = mutableMapOf(
-        "description" to description,
-        "amount" to amount.toString(),
-        "category" to category,
-        "date" to parsedDate.toString()
-    )
+    val expense = Expense(description, amount, category, parsedDate)
 
     expenses.add(expense)
-
 }
 
 fun viewAllExpenses() {
-    if(printIfEmpty())
-        return
-    var count = 1
+    if (printIfEmpty()) return
+
     for (expense in expenses) {
-        println("$count ---> $expense")
-        count++
+        println("Description: ${expense.description}, Category: ${expense.category}, Amount: ${expense.amount}, Date: ${expense.date}")
     }
 }
 
 fun filterByCategory() {
-    if(printIfEmpty()) return
+    if (printIfEmpty()) return
     var found = false
     print("Enter the category you want: ")
     val categoryChoice =
         readln().takeIf { it.isNotBlank() } ?: throw NullPointerException("PLease enter a description.")
 
     for (expense in expenses) {
-        if (expense.containsValue(categoryChoice)) {
-            println("(1) ---> $expense")
+        if (expense.category == categoryChoice) {
+            println("Description: ${expense.description}, Category: ${expense.category}, Amount: ${expense.amount}, Date: ${expense.date}")
             found = true
         }
     }
@@ -94,17 +87,16 @@ fun filterByCategory() {
 }
 
 fun filterByDate() {
-    if(printIfEmpty()) return
+    if (printIfEmpty()) return
     var found = false
-    val dateChoice: String
 
     print("What date would you like to see? ")
     val date = readln().takeIf { it.isNotBlank() } ?: throw NullPointerException("Date cannot be empty.")
-    dateChoice = LocalDate.parse(date).toString()
+    val dateChoice = LocalDate.parse(date)
 
     for (expense in expenses) {
-        if (expense.containsValue(dateChoice)) {
-            println("(1) --> $expense")
+        if (expense.date == dateChoice) {
+            println("Description: ${expense.description}, Category: ${expense.category}, Amount: ${expense.amount}, Date: ${expense.date}")
             found = true
         }
     }
@@ -114,7 +106,7 @@ fun filterByDate() {
 }
 
 fun filterByAmountRange() {
-    if(printIfEmpty()) return
+    if (printIfEmpty()) return
     print("Enter lower limit in the range: ")
     val lowerLimitRange =
         readln().toDoubleOrNull() ?: throw NumberFormatException("Lower Limit value should be entered and be numeric.")
@@ -124,8 +116,8 @@ fun filterByAmountRange() {
 
     for (expense in expenses) {
         if (lowerLimitRange >= 0 && upperLimitRange >= 0) {
-            if (expense["amount"]?.toDouble()!! in lowerLimitRange..upperLimitRange) {
-                println("(1) --> $expense")
+            if (expense.amount in lowerLimitRange..upperLimitRange) {
+                println("Description: ${expense.description}, Category: ${expense.category}, Amount: ${expense.amount}, Date: ${expense.date}")
             }
         } else {
             println("Values cannot be below zero.")
@@ -137,14 +129,14 @@ fun filterByAmountRange() {
 fun overallTotal(): Double {
     var total = 0.0
     for (expense in expenses) {
-        total += expense["amount"]?.toDouble()!!
+        total += expense.amount
     }
 
     return total
 }
 
 fun totalExpenseByCategory() {
-    if(printIfEmpty()) return
+    if (printIfEmpty()) return
     var total = 0.0
     var isFound = false
     print("Enter the category you want: ")
@@ -152,8 +144,8 @@ fun totalExpenseByCategory() {
         readln().takeIf { it.isNotBlank() } ?: throw NullPointerException("Category cannot be empty.")
 
     for (expense in expenses) {
-        if (expense.containsValue(categoryTotal)) {
-            total += expense["amount"]?.toDouble()!!
+        if (expense.category == categoryTotal) {
+            total += expense.amount
             isFound = true
         }
     }
@@ -166,11 +158,11 @@ fun totalExpenseByCategory() {
 }
 
 fun deleteExpense() {
-    if(printIfEmpty()) return
+    if (printIfEmpty()) return
     var index = 0
     for (expense in expenses) {
         index++
-        println("\t\t($index) --> $expense\n")
+        println("\t\t($index). Description: ${expense.description}, Category: ${expense.category}, Amount: ${expense.amount}, Date: ${expense.date}")
     }
 
     print("Enter the index of the expense you want to remove: ")
@@ -181,12 +173,14 @@ fun deleteExpense() {
     }
 }
 
-enum class Message(val text: String){
+enum class Message(val text: String) {
     EMPTY_LIST("Expense List Is Empty.")
 }
 
-fun printIfEmpty():Boolean{
-    if(expenses.isEmpty())
+fun printIfEmpty(): Boolean {
+    if (expenses.isEmpty()) {
         println(Message.EMPTY_LIST.text)
-    return true
+        return true
+    }
+    return false
 }
